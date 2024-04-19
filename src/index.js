789b6d797524055196778396acb5e3051043c95a
Tonkla W. <t0nk14.wib@gmail.com>
2024-04-19 21:45:02 +0000

    feat: mongoose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
 import router from './internal/route.js';
+import { connect } from './database/mongoose.js';
+
+dotenv.config();
+
+connect();
 
 const app = express();
 
@@ -10,6 +17,8 @@ app.use(cors());
 
 app.use("/api", router);
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log('Server is running on http://localhost:3000');
 });
